6c45bc42a0054b4a70979c549cc8384467fbd610
Luis Carlos B <luboteroa@unal.edu.co>
2021-10-23 15:25:39 -0500

    Program in and out

---
+++ FirstProgram.js
@@ -0,0 +1,24 @@
+var preguntas = ['¿Cuál es tu nombre? ',
+                 '¿Cuántos años tienes? ',
+                 '¿Lenguaje de programación favorito? '];
+
+var respuestas = [];
+
+function pregunta(i)
+{
+    process.stdout.write(preguntas[i]);
+}
+
+process.stdin.on('data', function(data)
+{
+    respuestas.push(data.toString().trim());
+
+    if(respuestas.length < preguntas.length)
+    {
+        pregunta(respuestas.length);
+    } else { 
+        process.exit();
+    }
+});
+
+pregunta(0);
